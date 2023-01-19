406eb78fb80c318fbf73d823ced9731ba09c700d
Yusuf Sultan <overcoder@protonmail.com>
2023-01-19 11:14:43 +0400

    feat: implement landing page

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: true,
+	bracketSameLine: true,
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 110,
+	useTabs: true,
+	arrowParens: 'avoid',
+	jsxSingleQuote: false,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: 'Inter',
+		},
+		colors: {
+			'text-primary': '#1F1F1F',
+			'text-secondary': '#545454',
+			'text-tetriary': '#A6A6A6',
+			'text-on-brand': '#F2F2F2',
+
+			'light-1': '#FFFFFF',
+			'light-2': '#F7F7F7',
+			'light-3': '#F0F0F0',
+			'light-4': '#E8E8E8',
+			'light-5': '#E0E0E0',
+
+			'primary-500': '#3F51B5',
+		},
+		container: {
+			center: true,
+			padding: '24px',
+			screens: {
+				DEFAULT: '1248px', // extra 48px to account for padding
+			},
+		},
+	},
+	plugins: [],
+};
